bdfa8e0e8e15168b3c5fd9d7c3c3ea68a810b1dc
VAIO <vijaykumar.karur@gmail.com>
2022-09-19 17:14:15 +0530

    inventory-service: dto - InventoryRequestDTO dto - InventoryResponseDTO

---
+++ inventory-service/src/main/java/com/learntocode/inverntoryservice/dto/InventoryRequestDTO.java
@@ -0,0 +1,15 @@
+package com.learntocode.inverntoryservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class InventoryRequestDTO {
+    String skuCode;
+    Integer quantity;
+}
---
+++ inventory-service/src/main/java/com/learntocode/inverntoryservice/dto/InventoryResponseDTO.java
@@ -0,0 +1,16 @@
+package com.learntocode.inverntoryservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class InventoryResponseDTO {
+    Long id;
+    String skuCode;
+    Integer quantity;
+}
