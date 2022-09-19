713abc1b32e124762d21bb20215be991c3b3cf6b
VAIO <vijaykumar.karur@gmail.com>
2022-09-19 23:13:26 +0530

    order-service: model - Order and OrderLineItem entities

---
+++ order-service/src/main/java/com/learntocode/orderservice/model/Order.java
@@ -0,0 +1,21 @@
+package com.learntocode.orderservice.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String orderNumber;
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<OrderLineItem> orderLineItemList;
+}
---
+++ order-service/src/main/java/com/learntocode/orderservice/model/OrderLineItem.java
@@ -0,0 +1,24 @@
+package com.learntocode.orderservice.model;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderLineItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String skuCode;
+    private BigDecimal price;
+    private Integer quantity;
+}
