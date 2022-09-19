f1de63797e026f138aed5188801afb030f379bfa
VAIO <vijaykumar.karur@gmail.com>
2022-09-19 16:32:16 +0530

    product-service: ProductRepository

---
+++ product-service/src/main/java/com/learntocode/productservice/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.learntocode.productservice.repository;
+
+import com.learntocode.productservice.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProductRepository extends MongoRepository<Product, String> {
+}
