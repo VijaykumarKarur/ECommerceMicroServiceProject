2e6ffb9a5626e8f3b6c174ce919ba4bc620f6d9c
VAIO <vijaykumar.karur@gmail.com>
2022-09-19 18:07:24 +0530

    inventory-service: exception - InventoryNotFoundException - custom exception exception - InventoryExceptionHandler - ControllerAdvice to handle exceptions

---
+++ inventory-service/src/main/java/com/learntocode/inverntoryservice/exception/InventoryExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.learntocode.inverntoryservice.exception;
+
+import com.learntocode.inverntoryservice.dto.ExceptionResponseDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class InventoryExceptionHandler {
+    @ExceptionHandler(InventoryNotFoundException.class)
+    public ResponseEntity<ExceptionResponseDTO> inventoryNotFoundExceptionHandler
+            (InventoryNotFoundException exception, WebRequest request){
+        ExceptionResponseDTO responseDTO = ExceptionResponseDTO
+                .builder()
+                .status(HttpStatus.NOT_FOUND)
+                .message(exception.getMessage())
+                .build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(responseDTO);
+    }
+}
