1e01ba1f12dd750c5e86a6b6cf08486c24563c13
VAIO <vijaykumar.karur@gmail.com>
2022-09-19 22:30:19 +0530

    product-service: service - ProductService and ProductServiceImpl - added methods to get Product by id, update and delete Product

--- product-service/src/main/java/com/learntocode/productservice/service/ProductService.java
+++ product-service/src/main/java/com/learntocode/productservice/service/ProductService.java
@@ -2,10 +2,15 @@ package com.learntocode.productservice.service;
 
 import com.learntocode.productservice.dto.ProductRequestDTO;
 import com.learntocode.productservice.dto.ProductResponseDTO;
+import com.learntocode.productservice.dto.ProductUpdateRequestDTO;
+import com.learntocode.productservice.exception.ProductNotFoundException;
 
 import java.util.List;
 
 public interface ProductService {
     ProductResponseDTO createProduct(ProductRequestDTO requestDTO);
     List<ProductResponseDTO> getAllProducts();
+    ProductResponseDTO getProductById(String id) throws ProductNotFoundException;
+    ProductResponseDTO updateProduct(ProductUpdateRequestDTO requestDTO) throws ProductNotFoundException;
+    void deleteProduct(String id) throws ProductNotFoundException;
 }
--- product-service/src/main/java/com/learntocode/productservice/service/ProductServiceImpl.java
+++ product-service/src/main/java/com/learntocode/productservice/service/ProductServiceImpl.java
@@ -2,45 +2,120 @@ package com.learntocode.productservice.service;
 
 import com.learntocode.productservice.dto.ProductRequestDTO;
 import com.learntocode.productservice.dto.ProductResponseDTO;
+import com.learntocode.productservice.dto.ProductUpdateRequestDTO;
+import com.learntocode.productservice.exception.ProductNotFoundException;
 import com.learntocode.productservice.model.Product;
 import com.learntocode.productservice.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService{
     private final ProductRepository productRepository;
 
+    /***
+     * Method to create Product
+     * @param requestDTO contains details needed to create Product
+     * @return ProductResponseDTO with saved Product details
+     */
     @Override
     public ProductResponseDTO createProduct(ProductRequestDTO requestDTO) {
-        Product product = Product
-                .builder()
-                .name(requestDTO.getName())
-                .description(requestDTO.getDescription())
-                .price(requestDTO.getPrice())
-                .build();
+        Product product = mapProductRequestToProduct(requestDTO);
         product = productRepository.save(product);
-
-        ProductResponseDTO responseDTO = ProductResponseDTO
-                .builder()
-                .id(product.getId())
-                .name(product.getName())
-                .description(product.getDescription())
-                .price(product.getPrice())
-                .build();
-
-        return responseDTO;
+        return mapProductToProductResponse(product);
     }
 
+    /***
+     * Method to get all Products
+     * @return List of ProductResponseDTO containing all Products
+     */
     @Override
     public List<ProductResponseDTO> getAllProducts() {
         List<Product> productList = productRepository.findAll();
         return productList.stream().map(this::mapProductToProductResponse).toList();
     }
 
+    /***
+     * Method to get Product based on id
+     * @param id id of the Product
+     * @return ProductResponseDTO containing the Product details of given Product id
+     * @throws ProductNotFoundException is thrown if Product is not found
+     */
+    @Override
+    public ProductResponseDTO getProductById(String id) throws ProductNotFoundException {
+        Optional<Product> product = productRepository.findById(id);
+        if(!product.isEmpty()){
+            throw new ProductNotFoundException("Product "+ id +" Not Found");
+        }
+        return mapProductToProductResponse(product.get());
+    }
+
+    /***
+     * Method to update Product
+     * @param requestDTO contains the Product details that need to be updated
+     * @return ProductResponseDTO containing the updated Product details
+     * @throws ProductNotFoundException is thrown if Product is not found
+     */
+    @Override
+    public ProductResponseDTO updateProduct(ProductUpdateRequestDTO requestDTO) throws ProductNotFoundException {
+        Optional<Product> productOptional = productRepository.findById(requestDTO.getId());
+        if(!productOptional.isEmpty()){
+            throw new ProductNotFoundException("Product "+ requestDTO.getId() +" Not Found");
+        }
+        Product product = productOptional.get();
+        if(Objects.nonNull(product.getName()) &&
+        !"".equalsIgnoreCase(product.getName())){
+            product.setName(product.getName());
+        }
+        if(Objects.nonNull(product.getDescription()) &&
+        !"".equalsIgnoreCase(product.getDescription())){
+            product.setDescription(product.getDescription());
+        }
+        if(Objects.nonNull(product.getPrice()) &&
+        !"".equalsIgnoreCase(product.getPrice().toString())){
+            product.setPrice(product.getPrice());
+        }
+        return mapProductToProductResponse(productRepository.save(product));
+    }
+
+    /***
+     * Method to delete Product given by id
+     * @param id of the Product
+     * @throws ProductNotFoundException is thrown if Product not found
+     */
+    @Override
+    public void deleteProduct(String id) throws ProductNotFoundException {
+        Optional<Product> optionalProduct = productRepository.findById(id);
+        if(optionalProduct.isEmpty()){
+            throw new ProductNotFoundException("Product "+ id + " Not Found");
+        }
+        productRepository.deleteById(id);
+    }
+
+    /***
+     * Method to map ProductRequestDTO to Product
+     * @param requestDTO ProductRequestDTO to be mapped
+     * @return Product
+     */
+    private Product mapProductRequestToProduct(ProductRequestDTO requestDTO){
+        return Product
+                .builder()
+                .name(requestDTO.getName())
+                .description(requestDTO.getDescription())
+                .price(requestDTO.getPrice())
+                .build();
+    }
+
+    /***
+     * Helper method to map Product to ProductResponseDTO
+     * @param product Product to be mapped
+     * @return ProductResponseDTO
+     */
     private ProductResponseDTO mapProductToProductResponse(Product product) {
         return ProductResponseDTO
                 .builder()
